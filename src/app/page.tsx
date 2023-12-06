b21ada08d097fef076ad2f79122a81400df66e18
Anh Tuan Bui <anhtuan128@gmail.com>
2023-12-07 06:22:48 +0700

    add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,156 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.scss";
+import { useState } from "react";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const goTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour 
+           in place of 'smooth' */
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <main id="" className={styles.main}>
+      <section className={styles.navigation}>
+        <ul>
+          <li>
+            <a href="#whatWeDo">What we do</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </section>
+      <section id="hero" className={styles.hero}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className={styles.mainImage}
+          src="/assets/images/tree.png"
+          alt="main picture"
+          width={1920}
+          height={1080}
         />
-      </div>
+        <div className={styles.mainText}>
+          <h1 className={styles.mainTextH1}>ANH TUAN BUI</h1>
+        </div>
+      </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <section id="whatWeDo" className={styles.secondSection}>
+        <div className={styles.triangle}>
+          <div className={styles.whiteStribe}></div>
+        </div>
+        <div className={styles.leadTriangle}></div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <div className={styles.leadTriangleTexts}>
+          <h2>Building website services</h2>
+          <p className={styles.leadTriangleTexts1}>
+            Prioritize intuitive, visually appealing designs with easy
+            navigation and responsive interfaces to enhance the user experience.
           </p>
-        </a>
-      </div>
+          <p className={styles.leadTriangleTexts2}>
+            Build websites that can grow with the client's needs, utilizing
+            scalable infrastructure and modular design principles for future
+            expansion.
+          </p>
+          <p className={styles.leadTriangleTexts3}>
+            Implement SEO best practices, including keyword optimization and
+            mobile responsiveness, to improve website visibility on search
+            engines.
+          </p>
+          <p className={styles.leadTriangleTexts4}>
+            Ensure website security through SSL certificates and secure coding
+            while optimizing performance with fast load times and content
+            delivery networks for a secure and efficient online presence.
+          </p>
+        </div>
+      </section>
+
+      <section id="services" className={styles.thirdSection}>
+        <Image
+          className={styles.mainImage}
+          src="/assets/images/coding.jpg"
+          alt="plants picture"
+          width={1920}
+          height={600}
+        />
+        <div className={styles.thirdSection}>
+          <div className={`${styles.thirdSectionInfo} ${styles.container}`}>
+            {/* <div className={styles.thirdSectionChild}> */}
+            <Image
+              className={styles.thirdSectionChildIcon}
+              src="/assets/icons/custom.png"
+              alt="plants picture"
+              width={50}
+              height={50}
+            />
+            <Image
+              className={styles.thirdSectionChildIcon}
+              src="/assets/icons/ecommerce.png"
+              alt="plants picture"
+              width={50}
+              height={50}
+            />
+            <Image
+              className={styles.thirdSectionChildIcon}
+              src="/assets/icons/seo.png"
+              alt="plants picture"
+              width={50}
+              height={50}
+            />
+            <h2>Custom Website Development</h2>
+            <h2>E-commerce Website Solutions</h2>
+            <h2>Website Redesign and Optimization</h2>
+            <p className={styles.leadTriangleTexts4}>
+              Craft tailored websites from scratch, aligning design and
+              functionality with client needs for a unique online presence.
+            </p>
+            {/* </div> */}
+            {/* <div className={styles.thirdSectionChild}> */}
+            <p className={styles.leadTriangleTexts4}>
+              Create robust online stores with seamless shopping experiences,
+              integrating secure payment gateways and scalable architecture.
+            </p>
+            {/* </div> */}
+            {/* <div className={styles.thirdSectionChild}> */}
+            <p className={styles.leadTriangleTexts4}>
+              Transform existing websites through redesign and performance
+              optimization, enhancing user experience and maximizing search
+              engine visibility.
+            </p>
+            {/* </div> */}
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.footer}>
+        <p>Copyright 2023 by Anh Tuan Bui</p>
+      </section>
+      <button className={styles.goTop} onClick={goTop} style={{ display: visible ? "inline" : "none" }}>
+        <Image
+          
+          src="/assets/icons/up-arrow.png"
+          alt="plants picture"
+          width={30}
+          height={30}
+        />
+      </button>
     </main>
-  )
+  );
 }
